19bca3832b2795abd866bd80697d385bd838182c
im-shubhamsharma <im_shubhamsharma@outlook.com>
2022-07-30 05:05:38 +0530

    added random meme option to project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Meme from "./component/Meme";
 
 export default function App() {
   return (
-    <>
+    <div className="main--container">
       <Header />
       <Meme />
-    </>
+    </div >
   );
 }
--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,6 +1,29 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+  function getMemeImage(){
+    let memesArray = allMemeImages.data.memes;
+    let randomNumber = Math.floor(Math.random() * memesArray.length);
+    let randomMemeUrl = memesArray[randomNumber].url;
+
+    setMeme( prevMeme => ({
+      ...prevMeme,
+      randomImage: randomMemeUrl
+    }))
+  }
+
+  
+
   return (
     <div className="form--section">
       <form className="form--input">
@@ -8,7 +31,11 @@ export default function Meme() {
         <input />
       </form>
 
-      <button className="form--button">Get an new meme image</button>
+      <button onClick={getMemeImage} className="form--button">
+        Get an new meme image
+      </button>
+
+      <img className="form--image" src={meme.randomImage} />
     </div>
   );
 }
